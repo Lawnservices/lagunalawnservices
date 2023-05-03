b7d4589039f6b0ffc2f3b0069e993b6127500ce9
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-05-03 13:32:10 -0400

    fix

--- javascript/app.js
+++ javascript/app.js
@@ -1,21 +1,29 @@
 const saluda = document.getElementById('saje');
 const sola = document.getElementById('men');
+const foto = document.getElementById('foto1');
+ 
+ 
  
 
 document.addEventListener('scroll', () =>{
   var scroll_position = window.scrollY;
   if(scroll_position > 600){
+    
     saje.style.backgroundColor = '#226654';
     saje.style.borderBottom = '4px solid #036';
-     men.style.color = 'blue'; 
-   
+    men.style.color = 'blue'; 
+    foto1.style.border = '4px solid #036';
     
        } else{
     saje.style.backgroundColor = '#036';
+    
     men.style.color = '#fff';
+    foto1.style.border = 'none';
     
     
     }
 });
+
+ 
 
  
