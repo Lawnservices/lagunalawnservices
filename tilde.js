d4d7439a318ef4aadb5d5e6f9a5a033339b3a33c
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-04-18 20:11:29 -0400

    fix-web

---
+++ tilde.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',function(){
+const mas = document.getElementById('tilde');
+
+document.addEventListener('scroll', () => {
+let scroll_position = window.scrollY;
+if(scroll_position > 600){
+  mas.style.width = '300px';
+  mas.style.height = '200px';
+} else{
+  mas.style.width = '0px';
+  mas.style.height = '0px';
+}
+
+});
+
+ 
+
+ 
+
+
+});
