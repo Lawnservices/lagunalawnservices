14d964c99b783661f48a7411a304face8aa1dae3
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-01-18 12:07:08 -0500

    fix-web

--- tilde.js
+++ tilde.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
 const mas = document.getElementById('tilde');
-
 document.addEventListener('scroll', () => {
 let scroll_position = window.scrollY;
 if(scroll_position > 600){
@@ -10,12 +9,7 @@ if(scroll_position > 600){
   mas.style.width = '0px';
   mas.style.height = '0px';
 }
-
+});
 });
 
  
-
- 
-
-
-});
