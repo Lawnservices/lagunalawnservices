ab60dadb0b0b24cba1ff6abd51027743783bee78
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-12-04 08:03:35 -0500

    fix_web

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-  setTimeout(() => {
+document.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
       console.log('%c¡Detente!', 'font-size: 40px; color: red; font-weight: bold;');
       console.log('%cEsta función del navegador está pensada para desarrolladores. Si alguien te indicó que copiaras y pegaras algo aquí para "activar funciones" o "hackear" algo, es un fraude. ¡No lo hagas!', 'font-size: 18px; color:red;');
    
@@ -7,3 +9,25 @@
 
     const fecha = new Date();
     console.log('%Inicializado el sitio de Miguel Lawn Services el: ' + fecha.toString());
+
+    document.querySelectorAll('a').forEach(link => {
+
+      link.addEventListener('mouseenter', () => {
+       link.style.border = '0.1rem solid blue';
+       document.getElementById('llamar').style.color = '#fff';
+       document.getElementById('navidad').textContent = 'Merry Christmas';
+      });
+
+      link.addEventListener('mouseleave', () => {
+       link.style.border = 'none';
+       document.getElementById('llamar').style.color = '#000';
+       document.getElementById('navidad').textContent = 'Lawn Mowing';
+      
+      });     
+
+
+
+      
+    });
+
+});
