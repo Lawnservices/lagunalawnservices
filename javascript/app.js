c4ae3e1a79f46498f042954d01aa13526a9160cb
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-03-26 08:11:18 -0400

    fix-web

--- javascript/app.js
+++ javascript/app.js
@@ -21,14 +21,14 @@ document.addEventListener('scroll', () =>{
 function myFunction(){
   document.getElementById('titulo').style.color = '#fff';
   document.getElementById('titulo').style.fontSize = '3rem';
-  document.getElementById('mase').style.backgroundImage = 'url("https://lagunalawnservice.com/img/galeria9.jpg")';
+  document.getElementById('mase').style.backgroundImage = 'url("https://mlaguna.pythonanywhere.com/static/uploads/36e903cf-1dcf-405d-a98b-8a85cd2532e9.jpg")';
    
 };
 
 function Photo(){
-  document.getElementById('titulo').style.color = '#299684';
+  document.getElementById('titulo').style.color = '#061125';
   document.getElementById('titulo').style.fontSize = '3rem';
-  document.getElementById('mase').style.backgroundImage = 'url("https://lagunalawnservice.com/img/galeria13.jpg")';
+  document.getElementById('mase').style.backgroundImage = 'url("https://mlaguna.pythonanywhere.com/static/uploads/589997df-dc5a-484b-bbcc-bdee87f5cdae.jpg")';
    
 };
 
