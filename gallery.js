4bc2c63b7e6a61018aeafbd7ceb71ffa8d3c5837
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-08-04 19:43:30 -0400

    fix

--- gallery.js
+++ gallery.js
@@ -1,13 +1,36 @@
+// fetch("https://www.creantunegocio.com/api/images")
+//   .then(res => res.json())
+//   .then(data => {
+//     const gallery = document.getElementById("gallery");
+
+//     data.forEach(img => {
+//       const image = document.createElement("img");
+//       image.src = "https://www.creantunegocio.com/static/uploads/" + img.filename;
+//       gallery.appendChild(image);
+//     });
+//   })
+//   .catch(err => console.error(err));
+ 
 fetch("https://www.creantunegocio.com/api/images")
-  .then(res => res.json())
-  .then(data => {
+.then(res => {
+    if (!res.ok) throw new Error("Error cargando imágenes");
+    return res.json();
+})
+.then(data => {
+
     const gallery = document.getElementById("gallery");
 
     data.forEach(img => {
-      const image = document.createElement("img");
-      image.src = "https://www.creantunegocio.com/static/uploads/" + img.filename;
-      gallery.appendChild(image);
+
+        const image = document.createElement("img");
+
+        image.src = `https://www.creantunegocio.com/static/uploads/${img.filename}`;
+
+        image.loading = "lazy";
+
+        gallery.appendChild(image);
+
     });
-  })
-  .catch(err => console.error(err));
- 
+
+})
+.catch(err => console.error(err));
