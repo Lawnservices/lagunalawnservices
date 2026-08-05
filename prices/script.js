64980c37f7f5361be111e1adca65a18907389fe1
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-08-05 11:16:41 -0400

    fix

--- prices/script.js
+++ prices/script.js
@@ -23,3 +23,13 @@
         }
 
         loadServices();
+
+
+        // navegacion
+
+ 
+function toggleMenu() {
+  const menu = document.querySelector(".nav-links");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+ 
